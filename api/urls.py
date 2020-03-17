a24ac5af58a9db5ea21ed9a0fc1e7242d20d5cad
Praful-a <prafuldubey27@gmail.com>
2020-03-17 21:55:00 +0530

    First commit

---
+++ api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, 'hello-viewset')
+router.register('profile', views.UserProfileViewSet)
+router.register('login', views.LoginViewSet, 'login')
+router.register('feed', views.UserProfileFeedViewSet)
+
+urlpatterns = [
+    path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls))
+]
